a099d36d067b00e81921d9cb7341622571be0386
Konstantin_Stepanov <ksstepanov_92@gmail.com>
2012-11-25 02:57:53 +0400

    OpenCV experiment. Shows image in window.

--- OpenCVHello.pro
+++ OpenCVHello.pro
@@ -16,3 +16,19 @@ SOURCES += main.cpp\
 HEADERS  += mainwindow.h
 
 FORMS    += mainwindow.ui
+
+LIBS+=`pkg-config opencv --cflags --libs`
+
+
+
+INCLUDEPATH+=\usr\local\lib\include\
+
+win32:CONFIG(release, debug|release): LIBS += -L$$PWD/../../../usr/local/lib/release/ -lopencv_core
+else:win32:CONFIG(debug, debug|release): LIBS += -L$$PWD/../../../usr/local/lib/debug/ -lopencv_core
+else:symbian: LIBS += -lopencv_core
+else:unix: LIBS += -L$$PWD/../../../usr/local/lib/ -lopencv_core
+
+
+
+INCLUDEPATH += $$PWD/../../../usr/local/include
+DEPENDPATH += $$PWD/../../../usr/local/include
--- main.cpp
+++ main.cpp
@@ -1,5 +1,12 @@
 #include <QtGui/QApplication>
 #include "mainwindow.h"
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <iostream>
+
+
+using namespace cv;
+using namespace std;
 
 int main(int argc, char *argv[])
 {
@@ -7,5 +14,28 @@ int main(int argc, char *argv[])
     MainWindow w;
     w.show();
     
-    return a.exec();
+    //return a.exec();
+
+
+    //if( argc != 2)
+    //{
+    // cout <<" Usage: display_image ImageToLoadAndDisplay" << endl;
+    // return -1;
+    //}
+
+    Mat image;
+    image = imread("frame1.ppm", CV_LOAD_IMAGE_COLOR);   // Read the file
+
+    if(! image.data )                              // Check for invalid input
+    {
+        cout <<  "Could not open or find the image" << std::endl ;
+        return -1;
+    }
+
+    namedWindow( "Display window", CV_WINDOW_AUTOSIZE );// Create a window for display.
+    imshow( "Display window", image );                   // Show our image inside it.
+
+    waitKey(0);                                          // Wait for a keystroke in the window
+    return 0;
+
 }
